e60793d2d4d7bb06ed18cd2b3a30c48226264990
Ancient <sebastian.merkl@bloodirony.com>
2021-05-23 23:27:58 +0200

    added a flagManager for flag handling

---
+++ public/src/flagManager.js
@@ -0,0 +1,31 @@
+export default class Flagmanager{
+    constructor(){
+        this.flags = [];
+    }
+
+    setFlag(_flagId, _value){
+        let f = this.getFlagById(_flagId);
+        if(f === null){
+            console.log("creating new flag: " + _flagId)
+            this.flags.push({
+                id: _flagId,
+                value: _value
+            });
+        }else{
+            f.value = _value;
+        }
+    }
+
+    getFlagById(_flagId){
+        let r = this.flags.filter((e) => {return e.id === _flagId});
+        if(r.length > 0){
+            return r[0];
+        }else{
+            return null;
+        }
+    }
+
+    removeFlag(_flagId){
+        this.flags = this.flags.filter((e) => {return e.id !== _flagId});
+    }
+}
